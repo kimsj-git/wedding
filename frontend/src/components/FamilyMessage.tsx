5a17d453b267bf777bb740fc09045f23386f1254
Suhjeong Kim <kimsj.py@gmail.com>
2023-09-17 00:06:55 +0900

    feat: [FE] 날짜, 위치, 메시지 추가

--- frontend/src/components/FamilyMessage.tsx
+++ frontend/src/components/FamilyMessage.tsx
@@ -5,15 +5,38 @@ const FamilyMessage: React.FC<{}> = () => {
     "안녕하십니까,",
     "신랑 신희창, 신부 김서정 입니다.",
     "저희 두 사람이 한 가족이 되는 자리에 함께 하셔서 축복해주시면 더 없이 감사하겠습니다.",
-    "신한수 최미숙의 장남 신희창",
-    "김헌식 정명숙의 장녀 김서정",
   ];
   return (
-    <div style={{padding: "2rem", paddingBottom: "5rem"}}>
-      <img src={elegantLeafImg} width="15%" style={{padding: "1rem 0"}} />
+    <div style={{ padding: "2rem", paddingBottom: "5rem" }}>
+      <img src={elegantLeafImg} width="15%" style={{ padding: "1rem 0" }} />
       {ourMessage.map((line) => (
         <p style={{ textAlign: "center", wordBreak: "keep-all" }}>{line}</p>
       ))}
+
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <p style={{ fontSize: "1.2rem", marginBottom: "0.5rem" }}>신한수 최미숙</p>
+        <p style={{ marginLeft: "0.5rem",  marginBottom: "0.5rem" }}>의 장남</p>
+        <p style={{ fontSize: "1.2rem", marginLeft: "1rem",marginBottom: "0.5rem" }}>희창</p>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <p style={{ color: "black",fontSize: "1.2rem", marginTop: "0.5rem" }}>김헌식 정명숙</p>
+        <p style={{ marginLeft: "0.5rem",marginTop: "0.5rem" }}>의 장녀</p>
+        <p style={{ fontSize: "1.2rem", marginLeft: "1rem",marginTop: "0.5rem" }}>서정</p>
+      </div>
     </div>
   );
 };
--- frontend/src/pages/InvitationPage.tsx
+++ frontend/src/pages/InvitationPage.tsx
@@ -5,6 +5,10 @@ import FamilyMessage from "../components/FamilyMessage";
 const InvitagionPage: React.FC<{}> = () => {
   return (
     <Layout>
+      <div style={{fontSize: "1.2rem"}}>
+        <p style={{color: "black", marginBottom: "0"}}>2023년 10월 21일 토요일 오후 12시</p>
+        <p style={{color: "black", fontSize: "1.4rem", margin: "0.5rem 0"}}>필경재 충효당</p>
+      </div>
       <FamilyMessage/>
       <Location/>
     </Layout>
