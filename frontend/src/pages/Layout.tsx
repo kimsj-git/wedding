5a108069a75b16a1cdbae4f264be51e3ed0af975
Suhjeong Kim <kimsj.py@gmail.com>
2023-09-26 22:43:34 +0900

    feat: [FE] 랜딩 애니메이션 추가 transition opacity 0 -> 1

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 
 import green5 from "../assets/images/green_5.jpg";
 import flowerImg from "../assets/icons/flower.png";
@@ -9,19 +9,31 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const [showImage, setShowImage] = useState(false);
+
+  useEffect(() => {
+    const showImageTimer = setTimeout(() => {
+      setShowImage(true);
+    }, 800);
+    return () => clearTimeout(showImageTimer);
+  }, []);
+
   return (
     <>
-      <div className="header-text">
-        <img src={flowerImg} width="15%" style={{ animation: "fadein 3s" }} />
-        <p>신희창</p>
-        <p>김서정</p>
-        <p>결혼합니다</p>
+      <div className={`layout ${showImage ? "show" : ""}`}>
+        <div className="header-text">
+          <img src={flowerImg} width="15%" style={{ animation: "fadein 3s" }} />
+          <p>신희창</p>
+          <p>김서정</p>
+          <p>결혼합니다</p>
+        </div>
+        <div className="container">
+          <img src={green5} width="100%" />
+        </div>
+
+        {children}
+        <PhotoGallery />
       </div>
-      <div>
-        <img src={green5} width="100%" />
-      </div>
-      {children}
-      <PhotoGallery />
     </>
   );
 };
