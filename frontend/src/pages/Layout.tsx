b3952a0db3418f4e07055b7f531050e802cccef9
Suhjeong Kim <kimsj.py@gmail.com>
2023-09-26 22:54:25 +0900

    feat: [FE] 랜딩시 로딩 애니메이션 적용

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -3,37 +3,53 @@ import { ReactNode, useEffect, useState } from "react";
 import green5 from "../assets/images/green_5.jpg";
 import flowerImg from "../assets/icons/flower.png";
 import PhotoGallery from "../components/PhotoGallery";
+import Loader from "../components/Loader";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const [isLoad, setIsLoad] = useState(false);
   const [showImage, setShowImage] = useState(false);
 
   useEffect(() => {
-    const showImageTimer = setTimeout(() => {
-      setShowImage(true);
-    }, 800);
-    return () => clearTimeout(showImageTimer);
-  }, []);
+    const loadTimer = setTimeout(() => {
+      setIsLoad(true);
+    }, 1500);
+
+    if (isLoad) {
+      const showImageTimer = setTimeout(() => {
+        setShowImage(true);
+      }, 500);
+      return () => clearTimeout(showImageTimer);
+    }
+    return () => clearTimeout(loadTimer);
+  }, [isLoad]);
 
   return (
     <>
-      <div className={`layout ${showImage ? "show" : ""}`}>
-        <div className="header-text">
-          <img src={flowerImg} width="15%" style={{ animation: "fadein 3s" }} />
-          <p>신희창</p>
-          <p>김서정</p>
-          <p>결혼합니다</p>
-        </div>
-        <div className="container">
-          <img src={green5} width="100%" />
-        </div>
+      {!isLoad && <Loader />}
+      {isLoad && (
+        <div className={`layout ${showImage ? "show" : ""}`}>
+          <div className="header-text">
+            <img
+              src={flowerImg}
+              width="15%"
+              style={{ animation: "fadein 3s" }}
+            />
+            <p>신희창</p>
+            <p>김서정</p>
+            <p>결혼합니다</p>
+          </div>
+          <div className="container">
+            <img src={green5} width="100%" />
+          </div>
 
-        {children}
-        <PhotoGallery />
-      </div>
+          {children}
+          <PhotoGallery />
+        </div>
+      )}
     </>
   );
 };
