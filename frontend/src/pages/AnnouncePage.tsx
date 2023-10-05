5d04df7e5b2387d0fa7be31b6fbe516005721b15
Suhjeong Kim <kimsj.py@gmail.com>
2023-10-05 21:07:10 +0900

    design: [FE] AnnouncePage 날짜 추가

--- frontend/src/pages/AnnouncePage.tsx
+++ frontend/src/pages/AnnouncePage.tsx
@@ -4,6 +4,11 @@ import GreetingMessage from "../components/GreetingMessage";
 const AnnouncePage = () => {
   return (
     <Layout>
+      <div style={{ fontSize: "1.2rem" }}>
+        <p style={{ color: "black", marginBottom: "0" }}>
+          2023년 10월 28일 토요일 오후 12시
+        </p>
+      </div>
       <GreetingMessage />
     </Layout>
   );
